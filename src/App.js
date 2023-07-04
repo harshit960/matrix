77fbb77ebb356f686ae1cf0817078b162b0590b4
harshit960 <raj.harshit960@gmail.com>
2023-07-04 16:55:27 +0530

    ch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const CURSOR_CLASS_NAME = 'custom-type-animation-cursor';
 function App() {
   return (
     <div className="bg-black text-gray-300 font-monaco">
-      <div className='pg-1 grid overflow-y-auto	h-screen w-screen'>
+      <div className='pg-1 grid 	h-screen w-screen'>
         <div className=' navbar flex flex-row items-center justify-center z-20 bg-black'>
           <div className='basis-full flex items-center justify-center'>
             <img src='./logo.png' className='h-16  w-auto '></img></div>
@@ -18,7 +18,7 @@ function App() {
 
 
         <div className='relative flex items-center 
-        jjustify-center h-screen text-lg	'>
+        jjustify-center h-screen text-lg	overflow-y-auto'>
           <div className='z-10'>
             {/* <img src="./terminal.png" className=''></img> */}
             <div className='ml-10 mt-16 mr-10 '>
